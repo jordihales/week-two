6a1e45269b4f872047437e2e8ca657a1a8111d9a
jordihales <46314874+jordihales@users.noreply.github.com>
2020-06-01 10:57:17 +0100

    header update

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,28 +1,44 @@
 <template>
   <header class="py-6">
     <div class="container max-w-screen-lg">
-      <div class="flex">
+      <div class="flex items-center">
         <svg
-          class="pr-2"
           height="25"
           viewBox="0 0 75 65"
           fill="#000"
         >
           <path d="M37.59.25l36.95 64H.64l36.95-64z" />
         </svg>
+        <span class="mx-2">
+          <svg
+            viewBox="0 0 25 25"
+            width="32"
+            height="32"
+            stroke="#e6e6e6"
+            stroke-width="1"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            fill="none"
+            shape-rendering="geometricPrecision"
+          >
+            <path d="M16.88 3.549L7.12 20.451"></path>
+          </svg>
+        </span>
         <svg
-          viewBox="0 0 24 24"
-          width="32"
-          height="32"
-          stroke="#e6e6e6"
-          stroke-width="1"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          fill="none"
-          shape-rendering="geometricPrecision"
+          fill="000"
+          viewbox="0 0 25 25"
+          height="30"
+          width="30"
         >
-          <path d="M16.88 3.549L7.12 20.451"></path>
+          <g>
+            <circle
+              cx="15"
+              cy="15"
+              r="15"
+            />
+          </g>
         </svg>
+        <h2 class="text-md pl-4">Jordi Hales</h2>
       </div>
     </div>
   </header>
