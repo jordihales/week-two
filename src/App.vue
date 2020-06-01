9d20a60254c15a66d7a8c07c4002792621dfc5f5
jordihales <46314874+jordihales@users.noreply.github.com>
2020-06-01 13:48:28 +0100

    add top nav

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <Header></Header>
+    <TopNav></TopNav>
   </div>
 </template>
 
 <script>
 import Header from "./components/Header.vue";
+import TopNav from "./components/TopNav.vue";
 
 export default {
   name: "App",
   components: {
-    Header
+    Header,
+    TopNav
   }
 };
 </script>
---
+++ src/components/TopNav.vue
@@ -0,0 +1,34 @@
+<template>
+  <nav class="w-full border-solid border-b relative">
+    <div class="container max-w-screen-lg">
+      <ul class="text-sm text-primary flex items-center mb-4">
+        <li class="active-item mr-6 hover:text-black transition ease-in-out duration-300"><a href="#">Overview</a></li>
+        <li class="mr-6 hover:text-black transition ease-in-out duration-300"><a href="#">Projects</a></li>
+        <li class="mr-6 hover:text-black transition ease-in-out duration-300"><a href="#">Integrations</a></li>
+        <li class="mr-6 hover:text-black transition ease-in-out duration-300"><a href="#">Activity</a></li>
+        <li class="mr-6 hover:text-black transition ease-in-out duration-300"><a href="#">Domains</a></li>
+        <li class="mr-6 hover:text-black transition ease-in-out duration-300"><a href="#">Usage</a></li>
+        <li class="hover:text-black transition ease-in-out duration-300"><a href="#">Settings</a></li>
+      </ul>
+
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+a:active {
+  :after {
+    content: "";
+    position: absolute;
+    height: 2px;
+    background: #000;
+    width: 50px;
+    bottom: 0;
+    left: 0;
+  }
+}
+</style>
