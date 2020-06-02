d0c3f62ddd0b35cc6d6b43cff5489433ac6d58d3
jordihales <46314874+jordihales@users.noreply.github.com>
2020-06-02 14:27:53 +0100

    build out projects section

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -20,17 +20,31 @@
           <div class="ml-6">
             <div class="flex items-center mb-4">
               <h2 class="text-4xl font-semibold mr-4">Jordi Hales</h2>
-              <p class="uppercase bg-gray-100 py-1 px-2 rounded-full border-solid border-secondary border text-xs">Hobby</p>
+              <p class="uppercase bg-gray-100 px-2 rounded-full border-solid border-secondary border text-xs">Hobby</p>
             </div>
             <p class="uppercase text-primary text-xs mb-2">Git Integration</p>
-            <p class="font-medium text-sm">jordihales</p>
+            <div class="flex items-center">
+              <svg
+                width="16"
+                height="16"
+                viewbox="0 0 16 16"
+              >
+                <path
+                  d="M7 .175c-3.872 0-7 3.128-7 7 0 3.084 2.013 5.71 4.79 6.65.35.066.482-.153.482-.328v-1.181c-1.947.415-2.363-.941-2.363-.941-.328-.81-.787-1.028-.787-1.028-.634-.438.044-.416.044-.416.7.044 1.071.722 1.071.722.635 1.072 1.641.766 2.035.59.066-.459.24-.765.437-.94-1.553-.175-3.193-.787-3.193-3.456 0-.766.262-1.378.721-1.881-.065-.175-.306-.897.066-1.86 0 0 .59-.197 1.925.722a6.754 6.754 0 0 1 1.75-.24c.59 0 1.203.087 1.75.24 1.335-.897 1.925-.722 1.925-.722.372.963.131 1.685.066 1.86.46.48.722 1.115.722 1.88 0 2.691-1.641 3.282-3.194 3.457.24.219.481.634.481 1.29v1.926c0 .197.131.415.481.328C11.988 12.884 14 10.259 14 7.175c0-3.872-3.128-7-7-7z"
+                  fill="#000"
+                  fill-rule="non-zero"
+                >
+                </path>
+              </svg>
+              <p class="font-medium text-sm ml-1">jordihales</p>
+            </div>
           </div>
         </div>
       </div>
       <div class="w-1/2">
         <div class="flex justify-end">
-          <button class="text-sm text-primary border-solid border-secondary border rounded-md px-8 py-2 mr-6">Invite Team</button>
-          <button class="text-sm text-white bg-black border-solid border-black border rounded-md px-8 py-2">Import Project</button>
+          <button class="text-sm text-primary border-solid border-secondary border rounded-md px-6 py-2 mr-6">Invite Team</button>
+          <button class="text-sm text-white bg-black border-solid border-black border rounded-md px-6 py-2">Import Project</button>
         </div>
       </div>
     </div>
--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,7 +1,250 @@
 <template>
   <div class="container max-w-screen-lg">
-    <div class="relative">
-      <div class="card w-1/2 bg-white rounded-md h-64 -mt-8">
+    <div class="relative flex">
+      <div class="w-1/2">
+        <div class="card bg-white rounded-md -mt-8">
+          <div class="flex items-center relative">
+            <h2 class="font-semibold text-xl p-6">project-one</h2>
+            <button class="text-sm absolute right-0 text-primary border-solid border-secondary border rounded-md px-4 py-1 mr-6">Visit</button>
+          </div>
+          <div class="px-6 pb-4 flex items-center">
+            <svg
+              fill="#50e3c2"
+              viewbox="0 0 10 10"
+              height="10"
+              width="10"
+            >
+              <g>
+                <circle
+                  cx="5"
+                  cy="5"
+                  r="5"
+                />
+              </g>
+            </svg>
+            <a
+              class="text-sm ml-4"
+              href="#"
+            >project-one.now.sh</a>
+            <p class="ml-2 bg-gray-100 px-2 rounded-full border-solid border-secondary border text-xs">Production</p>
+            <p class="text-primary text-sm ml-2">2h</p>
+          </div>
+          <div class="px-6 pb-4 flex items-center">
+            <svg
+              fill="#50e3c2"
+              viewbox="0 0 10 10"
+              height="10"
+              width="10"
+            >
+              <g>
+                <circle
+                  cx="5"
+                  cy="5"
+                  r="5"
+                />
+              </g>
+            </svg>
+            <a
+              class="text-sm ml-4"
+              href="#"
+            >project-one-c14qpnbum.now.sh</a>
+            <p class="ml-2 bg-gray-100 px-2 rounded-full border-solid border-secondary border text-xs">Latest</p>
+            <p class="text-primary text-sm ml-2">2h</p>
+          </div>
+          <div class="border-t">
+            <div class="px-6 py-4 flex items-center">
+              <svg
+                width="16"
+                height="16"
+                viewbox="0 0 16 16"
+              >
+                <path
+                  d="M7 .175c-3.872 0-7 3.128-7 7 0 3.084 2.013 5.71 4.79 6.65.35.066.482-.153.482-.328v-1.181c-1.947.415-2.363-.941-2.363-.941-.328-.81-.787-1.028-.787-1.028-.634-.438.044-.416.044-.416.7.044 1.071.722 1.071.722.635 1.072 1.641.766 2.035.59.066-.459.24-.765.437-.94-1.553-.175-3.193-.787-3.193-3.456 0-.766.262-1.378.721-1.881-.065-.175-.306-.897.066-1.86 0 0 .59-.197 1.925.722a6.754 6.754 0 0 1 1.75-.24c.59 0 1.203.087 1.75.24 1.335-.897 1.925-.722 1.925-.722.372.963.131 1.685.066 1.86.46.48.722 1.115.722 1.88 0 2.691-1.641 3.282-3.194 3.457.24.219.481.634.481 1.29v1.926c0 .197.131.415.481.328C11.988 12.884 14 10.259 14 7.175c0-3.872-3.128-7-7-7z"
+                  fill="#000"
+                  fill-rule="non-zero"
+                >
+                </path>
+              </svg>
+              <a
+                class="text-sm ml-2"
+                href="#"
+              >jordihales/project-one</a>
+            </div>
+          </div>
+        </div>
+        <div class="card bg-white rounded-md mt-12">
+          <div class="flex items-center relative">
+            <h2 class="font-semibold text-xl p-6">project-two</h2>
+            <button class="text-sm absolute right-0 text-primary border-solid border-secondary border rounded-md px-4 py-1 mr-6">Visit</button>
+          </div>
+          <div class="px-6 pb-4 flex items-center">
+            <svg
+              fill="#50e3c2"
+              viewbox="0 0 10 10"
+              height="10"
+              width="10"
+            >
+              <g>
+                <circle
+                  cx="5"
+                  cy="5"
+                  r="5"
+                />
+              </g>
+            </svg>
+            <a
+              class="text-sm ml-4"
+              href="#"
+            >project-two.now.sh</a>
+            <p class="ml-2 bg-gray-100 px-2 rounded-full border-solid border-secondary border text-xs">Production</p>
+            <p class="text-primary text-sm ml-2">2h</p>
+          </div>
+          <div class="px-6 pb-4 flex items-center">
+            <svg
+              fill="#50e3c2"
+              viewbox="0 0 10 10"
+              height="10"
+              width="10"
+            >
+              <g>
+                <circle
+                  cx="5"
+                  cy="5"
+                  r="5"
+                />
+              </g>
+            </svg>
+            <a
+              class="text-sm ml-4"
+              href="#"
+            >project-two-c14qpnbum.now.sh</a>
+            <p class="ml-2 bg-gray-100 px-2 rounded-full border-solid border-secondary border text-xs">Latest</p>
+            <p class="text-primary text-sm ml-2">2h</p>
+          </div>
+          <div class="border-t">
+            <div class="px-6 py-4 flex items-center">
+              <svg
+                width="16"
+                height="16"
+                viewbox="0 0 16 16"
+              >
+                <path
+                  d="M7 .175c-3.872 0-7 3.128-7 7 0 3.084 2.013 5.71 4.79 6.65.35.066.482-.153.482-.328v-1.181c-1.947.415-2.363-.941-2.363-.941-.328-.81-.787-1.028-.787-1.028-.634-.438.044-.416.044-.416.7.044 1.071.722 1.071.722.635 1.072 1.641.766 2.035.59.066-.459.24-.765.437-.94-1.553-.175-3.193-.787-3.193-3.456 0-.766.262-1.378.721-1.881-.065-.175-.306-.897.066-1.86 0 0 .59-.197 1.925.722a6.754 6.754 0 0 1 1.75-.24c.59 0 1.203.087 1.75.24 1.335-.897 1.925-.722 1.925-.722.372.963.131 1.685.066 1.86.46.48.722 1.115.722 1.88 0 2.691-1.641 3.282-3.194 3.457.24.219.481.634.481 1.29v1.926c0 .197.131.415.481.328C11.988 12.884 14 10.259 14 7.175c0-3.872-3.128-7-7-7z"
+                  fill="#000"
+                  fill-rule="non-zero"
+                >
+                </path>
+              </svg>
+              <a
+                class="text-sm ml-2"
+                href="#"
+              >jordihales/project-two</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="w-1/2">
+        <div class="ml-24">
+          <div>
+            <h2 class="text-sm font-semibold -mt-8">Recent Activity</h2>
+          </div>
+          <div class="mt-4 py-6 border-b flex items-center">
+            <div class="mr-4">
+              <svg
+                fill="000"
+                viewbox="0 0 25 25"
+                height="30"
+                width="30"
+              >
+                <g>
+                  <circle
+                    cx="15"
+                    cy="15"
+                    r="15"
+                  />
+                </g>
+              </svg>
+            </div>
+            <div>
+              <p class="font-light text-sm">jordihales deployed to project-one (987bb74 in master) to production</p>
+            </div>
+            <div>
+              <p class="text-primary text-sm ml-2">2h</p>
+            </div>
+          </div>
+          <div class="py-6 border-b flex items-center">
+            <div class="mr-4">
+              <svg
+                fill="000"
+                viewbox="0 0 25 25"
+                height="30"
+                width="30"
+              >
+                <g>
+                  <circle
+                    cx="15"
+                    cy="15"
+                    r="15"
+                  />
+                </g>
+              </svg>
+            </div>
+            <div>
+              <p class="font-light text-sm">jordihales deployed to project-one (987bb74 in master) to production</p>
+            </div>
+            <div>
+              <p class="text-primary text-sm ml-2">2h</p>
+            </div>
+          </div>
+          <div class="py-6 border-b flex items-center">
+            <div class="mr-4">
+              <svg
+                fill="000"
+                viewbox="0 0 25 25"
+                height="30"
+                width="30"
+              >
+                <g>
+                  <circle
+                    cx="15"
+                    cy="15"
+                    r="15"
+                  />
+                </g>
+              </svg>
+            </div>
+            <div>
+              <p class="font-light text-sm">jordihales deployed to project-one (987bb74 in master) to production</p>
+            </div>
+            <div>
+              <p class="text-primary text-sm ml-2">2h</p>
+            </div>
+          </div>
+          <div class="py-6 border-b flex items-center">
+            <div class="mr-4">
+              <svg
+                fill="000"
+                viewbox="0 0 25 25"
+                height="30"
+                width="30"
+              >
+                <g>
+                  <circle
+                    cx="15"
+                    cy="15"
+                    r="15"
+                  />
+                </g>
+              </svg>
+            </div>
+            <div>
+              <p class="font-light text-sm">jordihales deployed to project-one (987bb74 in master) to production</p>
+            </div>
+            <div>
+              <p class="text-primary text-sm ml-2">2h</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
